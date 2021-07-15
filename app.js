fdbc3e54e91485e0fc9fdfdb2f496214a8c8be73
无锡肖奈 <even199843@outlook.com>
2021-07-15 11:24:37 +0800

    test

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("index.ejs");
 });
 
 app.listen(3000, () => {
